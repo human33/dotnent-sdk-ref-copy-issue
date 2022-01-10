b8c11ea481fef71b216b8916eb216a6de273f5b5
Daniil Shakir <daniil.shakir@arora.pro>
2022-01-10 12:53:26 +0300

    upd

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using CJE;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CJE.Event e = new Event();
+            Console.WriteLine("Hello World!" + e);
+        }
+    }
+}
